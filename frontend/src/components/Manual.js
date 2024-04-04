09d536db7857bac297b9769c704da50aa5cad226
silocat <dericle77@gmail.com>
2024-04-04 16:18:03 -0500

    frontend routing and manual integration

--- frontend/src/components/Manual.js
+++ frontend/src/components/Manual.js
@@ -3,7 +3,10 @@ import React from 'react';
 function Manual() {
     return (
         <div className="manual">
-            manual
+            <iframe title="link to CAPOPs manual" src="https://docs.google.com/document/d/1KHTTKmqW1DhnMfh7q4gUF8pJdvkGUtWhaOIYMB2dU4Y/preview"></iframe>
+            <a target="_blank" href="https://docs.google.com/document/d/1KHTTKmqW1DhnMfh7q4gUF8pJdvkGUtWhaOIYMB2dU4Y/">
+                
+            </a>
         </div>
     );
 }
